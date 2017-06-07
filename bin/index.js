84d17b5107261a441d3634b4066e46afdc84a73b
wmathes <fanatik@gmx.at>
2017-06-07 14:07:53 +0700

    feat(jpgc): initial release

---
+++ bin/index.js
@@ -0,0 +1,84 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const execFile = require('child_process').execFile;
+const readChunk = require('read-chunk');
+const imageType = require('image-type');
+const guetzli = require('guetzli');
+const program = require('commander');
+const chalk = require('chalk');
+
+const updateNotifier = require('update-notifier');
+const pkg = require('../package.json');
+
+updateNotifier({pkg}).notify();
+
+let files = [];
+
+function walk(path) {
+    const dirList = fs.readdirSync(path);
+    dirList.forEach(item => {
+        const stats = fs.statSync(path + '/' + item);
+        if (stats.isFile()) {
+            const imageTypeData = imageType(readChunk.sync(path + '/' + item, 0, 12));
+            if (imageTypeData) {
+                if (imageTypeData.ext === 'jpg') {
+                    files.push({
+                        file: path + '/' + item,
+                        size: stats.size
+                    });
+                }
+            }
+        }
+    });
+
+    if (program.recursive) {
+        dirList.forEach(item => {
+            if (fs.statSync(path + '/' + item).isDirectory()) {
+                walk(path + '/' + item);
+            }
+        });
+    }
+}
+
+program
+    .version(pkg.version || 'dev')
+    .option('-r, --recursive', 'Walk given directory recursively')
+    .option('-p, --path <path>', 'A path to crawl, defaults to current working directory')
+    .option('-q, --quality <value>', 'A quality value, by default 95')
+    .option('-f, --files <items>', 'the files you want to convert,split by \',\'', v => v.split(','))
+    .parse(process.argv);
+
+if (program.files) {
+    files = program.files.map(f => {
+        return {
+            file: f,
+            size: fs.statSync(f).size
+        };
+    });
+} else {
+    walk(process.path || process.cwd());
+}
+
+console.log(chalk.yellow('Found ' + files.length + ' image file(s) !'));
+
+const quality = program.quality || 95;
+
+files.forEach(item => {
+    const args = ['--quality', quality, item.file, item.file];
+    execFile(guetzli, args, err => {
+        if (err) {
+            throw err;
+        }
+
+        item.newSize = fs.statSync(item.file).size;
+        const percDiff = Math.round(((1 - item.newSize / item.size) * 100));
+
+        if (percDiff > 0) {
+            console.log(chalk.green(item.file + ' was compressed by ' + percDiff + "%!"));
+        }
+        else {
+            console.log(chalk.gray(item.file + ' was already compressed'));
+        }
+    });
+});
---
+++ readme.md
@@ -0,0 +1,53 @@
+# jpeg-compression-cli
+
+[![npm](https://img.shields.io/npm/v/@crazyfactory/jpeg-compression-cli.svg)](http://www.npmjs.com/package/@crazyfactory/jpeg-compression-cli)
+[![Build Status](https://travis-ci.org/crazyfactory/jpeg-compression-cli.svg?branch=master)](https://travis-ci.org/crazyfactory/jpeg-compression-cli)
+[![dependencies Status](https://david-dm.org/crazyfactory/jpeg-compression-cli/status.svg)](https://david-dm.org/crazyfactory/jpeg-compression-cli)
+[![devDependencies Status](https://david-dm.org/crazyfactory/jpeg-compression-cli/dev-status.svg)](https://david-dm.org/crazyfactory/jpeg-compression-cli?type=dev)
+[![semantic-release](https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg)](https://github.com/semantic-release/semantic-release)
+[![Greenkeeper badge](https://badges.greenkeeper.io/crazyfactory/jpeg-compression-cli.svg)](https://greenkeeper.io/)
+
+The CommandLine tool for converting jpg or png files to webp, recursively.
+
+## Awsome featurn ✧٩(ˊωˋ*)و✧
+This tool would search the jpg or png files by the head infomation with the files, so even the files without the ext name,they also would be found and be converted.
+
+so enjoy it !
+
+## Installation
+
+    $ npm i -g @crazyfactory/jpeg-compression-cli
+
+## Usage
+
+jpeg-compression-cli allow you to convert your images within a current directory, recursively or not. It will overwrite your files with the compressed version 
+
+After installation, just run command `jpgc` in ternimal.
+
+To convert all images within the current directory and subdirectoies, use the -r flag
+
+    $ jpgc -r
+    
+To choose a different folder provide it with -p flag
+
+    $ jpgc -p /var/my-images
+
+To convert the specific image files (assets/img.jpg in this example), you may run the following command.
+
+    $ jpgc -f assets/img.jpg
+    
+or
+
+    $ jpgc -f assets/img1.jpg,assets/img2.png
+
+for more help infomation, you could run the -h flag to check it out.
+
+    $ jpgc -h
+
+## License
+
+Copyright (c) 2017 Wolf T. Mathes for Crazy Factory Trading Co. Ltd.
+
+Licensed under the MIT license.
+
+See LICENSE for more info.
